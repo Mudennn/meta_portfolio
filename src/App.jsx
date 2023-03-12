1aa9e8b6839d62fb59accb95fd56ea7d701b1f04
Muden <mudennn@yahoo.com>
2023-03-12 21:54:01 +0800

    nav update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
+import { BrowserRouter } from "react-router-dom";
 
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
-const App = () =>  {
-
-
+const App = () => {
   return (
-    <div>
-      Muden Portfolio
-      <p>Suppppp kita buat 3d profile brooo!</p>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        {/* display star */}
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
